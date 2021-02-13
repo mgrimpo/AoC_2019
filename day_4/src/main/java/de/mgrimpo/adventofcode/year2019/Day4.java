d70ad337bf7b2f5639bdebc1cc37f46c8bda3f29
Marc Grimpo <mgrimpo.dev@gmail.com>
2021-02-13 14:25:21 +0100

    Add solution for day 4: puzzle 2

--- day_4/src/main/java/de/mgrimpo/adventofcode/year2019/Day4.java
+++ day_4/src/main/java/de/mgrimpo/adventofcode/year2019/Day4.java
@@ -1,17 +1,23 @@
 package de.mgrimpo.adventofcode.year2019;
 
+import java.util.function.Predicate;
+
 public class Day4 {
 
-  static int[] puzzleInputDay1 = new int[] {153517, 630395};
+  static int[] puzzleInput = new int[]{153517, 630395};
 
   public static void main(String[] args) {
-    int solutionDay1 = solveDay1(puzzleInputDay1);
+    int solutionPuzzle1 = solveDay4(puzzleInput, Day4::containsNeighboringPair);
     System.out.printf(
-        "There are %d possible passwords for the range of %d-%d\n",
-        solutionDay1, puzzleInputDay1[0], puzzleInputDay1[1]);
+        "Puzzle 1: There are %d possible passwords for the range of %d-%d\n",
+        solutionPuzzle1, puzzleInput[0], puzzleInput[1]);
+    int solutionPuzzle2 = solveDay4(puzzleInput, Day4::containsIsolatedPair);
+    System.out.printf(
+        "Puzzle 2: There are %d possible passwords for the range of %d-%d\n",
+        solutionPuzzle2, puzzleInput[0], puzzleInput[1]);
   }
 
-  static int solveDay1(int[] puzzleInputDay1) {
+  static int solveDay4(int[] puzzleInputDay1, Predicate<Integer> neighborRule) {
     int numberOfPossiblePasswords = 0;
     int currentCandidate = puzzleInputDay1[0];
     while (currentCandidate <= puzzleInputDay1[1]) {
@@ -20,7 +26,7 @@ public class Day4 {
         currentCandidate = nextPossibleCandidate(currentCandidate, greaterPredecessorDigitPosition);
         continue;
       }
-      if (containsIdenticalNeighbors(currentCandidate)) {
+      if (neighborRule.test(currentCandidate)) {
         numberOfPossiblePasswords++;
       }
       currentCandidate++;
@@ -28,10 +34,11 @@ public class Day4 {
     return numberOfPossiblePasswords;
   }
 
-   static int nextPossibleCandidate(int currentCandidate, int greaterPredecessorPosition) {
+  static int nextPossibleCandidate(int currentCandidate, int greaterPredecessorPosition) {
     String candidateString = Integer.toString(currentCandidate);
-    StringBuilder newCandidate = new StringBuilder(candidateString.substring(0, greaterPredecessorPosition + 1));
-    for (int i=0; i < 5 - greaterPredecessorPosition; i++) {
+    StringBuilder newCandidate =
+        new StringBuilder(candidateString.substring(0, greaterPredecessorPosition + 1));
+    for (int i = 0; i < 5 - greaterPredecessorPosition; i++) {
       newCandidate.append(candidateString.charAt(greaterPredecessorPosition));
     }
     return Integer.parseInt(newCandidate.toString());
@@ -42,7 +49,7 @@ public class Day4 {
    * that position. If no such digit is found returns -1
    *
    * @param currentCandidate the current candidate for a password, represented as a String of 6
-   *     digits
+   *                         digits
    * @return the position of the digit that is greater than its successor digit
    */
   static int findGreaterPredecessorDigit(int currentCandidate) {
@@ -55,7 +62,10 @@ public class Day4 {
     return -1;
   }
 
-  public static boolean containsIdenticalNeighbors(Integer passwordCandidate) {
+  /**
+   * Checks whether {@code passwordCandidate} contains two adjacent matching digits.
+   */
+  static boolean containsNeighboringPair(Integer passwordCandidate) {
     String passwordCandidateString = Integer.toString(passwordCandidate);
     for (int i = 0; i < passwordCandidateString.length() - 1; i++) {
       if (passwordCandidateString.charAt(i) == passwordCandidateString.charAt(i + 1)) {
@@ -63,5 +73,29 @@ public class Day4 {
       }
     }
     return false;
+  }
+
+  /**
+   * Checks whether {@code passwordCandidate} has two adjacent matching digits, where the two adjacent
+   * matching digits are not part of a larger group of matching digits.
+   *
+   * @param passwordCandidate
+   */
+  static boolean containsIsolatedPair(int passwordCandidate) {
+    String candidateString = Integer.toString(passwordCandidate);
+    return isolatedPairHelper(candidateString);
+  }
+
+  private static boolean isolatedPairHelper(String passwordCandidate) {
+    if (passwordCandidate.isEmpty()) return false;
+
+    var sameCharacterStreakLength =
+        passwordCandidate.chars()
+            .takeWhile(c -> c == passwordCandidate.charAt(0))
+            .count();
+    if (sameCharacterStreakLength == 2) {
+      return true;
+    }
+    return isolatedPairHelper(passwordCandidate.substring((int) sameCharacterStreakLength));
   }
 }
