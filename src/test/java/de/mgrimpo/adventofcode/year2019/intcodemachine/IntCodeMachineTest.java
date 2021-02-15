cde9520a10b49ca38d10a863f215847c7046c6ea
Marc Grimpo <mgrimpo.dev@gmail.com>
2021-02-15 17:17:06 +0100

    Fix incorrect opcode in IntCodeMachineTest

--- src/test/java/de/mgrimpo/adventofcode/year2019/intcodemachine/IntCodeMachineTest.java
+++ src/test/java/de/mgrimpo/adventofcode/year2019/intcodemachine/IntCodeMachineTest.java
@@ -12,13 +12,18 @@ public class IntCodeMachineTest {
   //  the value from the second parameter. Otherwise, it does nothing.
   @Test
   public void testJumpIfTrueInstruction() {
-    var exampleProgramWithJump = new int[]{115, 1, 6, -1, 0, 0, 99};
+    var exampleProgramWithJump = new int[]{1105, 1, 6, -1, 0, 0, 99};
     var jumpIfTrueInstruction = new JumpIfTrueInstruction(exampleProgramWithJump, 0);
-    assertEquals(jumpIfTrueInstruction.execute(exampleProgramWithJump).get(), 99);
-    var exampleProgramWithOutJump = new int[]{115, 0, 6, 1, 0, 0, 0, 99};
+    assertEquals(6, jumpIfTrueInstruction.execute(exampleProgramWithJump).get());
+
+    var exampleProgramWithOutJump = new int[]{1105, 0, 6, 1, 0, 0, 0, 99};
     jumpIfTrueInstruction = new JumpIfTrueInstruction(exampleProgramWithOutJump, 0);
     assertTrue(jumpIfTrueInstruction.execute(exampleProgramWithOutJump).isEmpty());
-    // TODO: test positional parameter mode
+
+    // the positional second parameter refers to the value 5 at index 3
+    var programWithPositionalParams = new int[]{105, 1, 3, 5, 99,};
+    jumpIfTrueInstruction = new JumpIfTrueInstruction(programWithPositionalParams, 0);
+    assertEquals(5, jumpIfTrueInstruction.execute(programWithPositionalParams).get());
   }
   //  Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
   //  Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
